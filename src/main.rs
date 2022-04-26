b428df4be3f5ea6063110d102cc71661aac71779
Steven Ferrer <steven.r.ferrer@gmail.com>
2022-04-26 21:45:29 +0800

    Added progress indicator

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,25 @@
-use std::io::{self, Write};
+use std::io::{self, stdout, stderr, Write};
 
 fn main() {
     let _ = make_ppm();
 }
 
 fn make_ppm() -> io::Result<()> {
-    let stdout = io::stdout();
-    let mut handle = stdout.lock();
+    let stdout = stdout();
+    let stderr = stderr();
+    let mut stdout = stdout.lock();
+    let mut stderr = stderr.lock();
 
     let image_width: i32 = 256;
     let image_height: i32 = 256;
 
-    handle.write_fmt(format_args!("P3\n{} {}\n255\n", image_width, image_height))?;
+    stdout.write_fmt(format_args!("P3\n{} {}\n255\n", image_width, image_height))?;
 
     let mut j = image_height - 1;
     while j >= 0 {
+        stderr.write_fmt(format_args!("\rScanlines remaining: {}", j))?;
+        stderr.flush()?;
+
         let mut i = 0;
         while i < image_width {
             let r = i as f32 / (image_width - 1) as f32;
@@ -25,13 +30,15 @@ fn make_ppm() -> io::Result<()> {
             let ig = (255.999 * g) as i32;
             let ib = (255.999 * b) as i32;
 
-            handle.write_fmt(format_args!("{} {} {}\n", ir, ig, ib))?;
+            stdout.write_fmt(format_args!("{} {} {}\n", ir, ig, ib))?;
 
             i += 1;
         }
 
         j -= 1;
     }
+
+    stderr.write_all(b"\nDone.\n")?;
 
     Ok(())
 }
